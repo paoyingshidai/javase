a6708fe8507ae320018d494bf8e947a32af493cf
Michael <13432849094@163.com>
2020-07-01 15:27:04 +0800

    添加 BiMapTest 测试

---
+++ guava/src/test/java/com/michael/javase/guava/BiMapTest.java
@@ -0,0 +1,47 @@
+package com.michael.javase.guava;
+
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+import com.google.common.collect.Maps;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.junit.Test;
+
+/**
+ * @auth Michael
+ */
+public class BiMapTest {
+
+    @Test
+    public void biMapTest() {
+        BiMap<String, User> map = Maps.synchronizedBiMap(HashBiMap.create());
+
+        User user = new User(12, "username");
+        map.put(user.getUsername(), user);
+
+        User ret = map.get(user.getUsername());
+        System.out.println(ret);
+        
+        user.setUsername("username");
+
+        String username = map.inverse().get(user);
+        System.out.println(username);
+
+    }
+
+    @Data
+    @AllArgsConstructor
+    class User {
+
+        private Integer age;
+        private String username;
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof User) {
+                return this.age == ((User) obj).getAge();
+            }
+            return false;
+        }
+    }
+}
