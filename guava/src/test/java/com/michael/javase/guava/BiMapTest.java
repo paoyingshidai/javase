be12742c878bfef91c04d875fb49cb864340877e
Michael <13432849094@163.com>
2020-07-01 15:48:10 +0800

    添加 BiMapTest 测试

--- guava/src/test/java/com/michael/javase/guava/BiMapTest.java
+++ guava/src/test/java/com/michael/javase/guava/BiMapTest.java
@@ -5,6 +5,7 @@ import com.google.common.collect.HashBiMap;
 import com.google.common.collect.Maps;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.junit.Test;
 
 /**
@@ -22,27 +23,23 @@ public class BiMapTest {
         User ret = map.get(user.getUsername());
         System.out.println(ret);
         
-        user.setUsername("username");
+        user.setUsername("user");
 
         String username = map.inverse().get(user);
         System.out.println(username);
 
     }
 
+    /**
+     * 需要重写 equals 和 hash 方法
+     */
     @Data
     @AllArgsConstructor
+    @EqualsAndHashCode(exclude = "username")
     class User {
 
         private Integer age;
         private String username;
 
-        // TODO 重写 equals 必须要重写 hash 方法
-        @Override
-        public boolean equals(Object obj) {
-            if (obj instanceof User) {
-                return this.age == ((User) obj).getAge();
-            }
-            return false;
-        }
     }
 }
