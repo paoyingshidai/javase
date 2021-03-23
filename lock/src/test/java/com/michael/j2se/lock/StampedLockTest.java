a2acbea4266b5162c57b3612807efed33c0e39df
Michael <13432849094@163.com>
2021-03-23 22:10:41 +0800

    StampedLockTest

--- lock/src/test/java/com/michael/j2se/lock/StampedLockTest.java
+++ lock/src/test/java/com/michael/j2se/lock/StampedLockTest.java
@@ -2,6 +2,9 @@ package com.michael.j2se.lock;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.locks.StampedLock;
 
 /**
@@ -9,14 +12,71 @@ import java.util.concurrent.locks.StampedLock;
  */
 public class StampedLockTest {
 
+    ExecutorService executorService = Executors.newFixedThreadPool(100);
 
     @Test
-    void stampedLockTest() {
+    void stampedLockTest() throws InterruptedException {
 
         StampedLock lock = new StampedLock();
+        final Counter counter = new Counter(lock);
 
-        lock.tryConvertToReadLock(0);
+        for (int i = 0; i < 10; i++) {
+            executorService.submit(() -> {
+                int j = 0;
+                while (j < 1000) {
+                    counter.add();
+                    j++;
+                }
+            });
+        }
 
+        executorService.submit(() -> {
+            int j = 0;
+            while (j < 1000) {
+                counter.print();
+                j++;
+            }
+        });
+
+        new CountDownLatch(1).await();
+    }
+
+
+
+    class Counter {
+
+        StampedLock lock;
+
+        int x = 0, y = 0;
+
+        public Counter(StampedLock lock) {
+            this.lock = lock;
+        }
+
+        public void add() {
+            long l = lock.writeLock();
+            try {
+                x++;
+                y++;
+            } finally {
+                lock.unlockWrite(l);
+            }
+        }
+
+        public void print() {
+            long l = lock.tryOptimisticRead();
+
+            try {
+                if (!lock.validate(l)) {
+                    l = lock.readLock();
+                    if (x != y) {
+                        System.out.println("x = " + x + " y = " + y);
+                    }
+                }
+            } finally {
+                lock.unlockRead(l);
+            }
+        }
     }
 
 }
